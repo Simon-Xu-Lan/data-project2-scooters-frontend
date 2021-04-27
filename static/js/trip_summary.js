73b87b55801d1fcd3d4968eee4e98082aaa8a3b3
Simon Xu <xulan20907@gmail.com>
2021-04-27 13:35:44 -0600

    Add two chart

--- static/js/trip_summary.js
+++ static/js/trip_summary.js
@@ -1,4 +1,4 @@
-d3.csv('static/data/trip_summary.csv').then((data) => {
+d3.csv('./static/data/trip_summary.csv').then((data) => {
   console.log(data);
   var tableBodyElement = d3.select('#summary_table').select('tbody');
   tableBodyElement.html('');
@@ -8,4 +8,49 @@ d3.csv('static/data/trip_summary.csv').then((data) => {
       row.append('td').text(value);
     });
   });
+});
+
+d3.json('http://scooters.pythonanywhere.com/api/trips/summary').then((data) => {
+  console.log(data);
+
+  // Pie chart
+  var trace1 = {
+    labels: data.data.map((row) => row.company),
+    values: data.data.map((row) => row.time_min),
+    type: 'pie',
+  };
+
+  var data1 = [trace1];
+  var layout1 = {
+    title: 'Scooter Usage',
+  };
+
+  Plotly.newPlot('pie', data1, layout1);
+
+  // Bubble chart
+  var trace2 = {
+    type: 'bubble',
+    x: data.data.map((row) => row.scooter_qty),
+    y: data.data.map((row) => Math.round(row.time_min / 60, 2)),
+    mode: 'markers',
+    marker: {
+      size: data.data.map((row) => row.distance_meter / 100000),
+      // color: props.sample[0].sample_values.map(
+      // (value) => `rgb(${value}, ${255 - value}, ${Math.random() * 255})`
+      // ),
+    },
+    text: data.data.map((row) => row.company),
+  };
+  var data2 = [trace2];
+
+  var layout2 = {
+    title: `Scooters`,
+    xaxis: { title: 'Scooter Quantity' },
+    yaxis: { title: 'Scooter Usage (Hrs)' },
+    showlegend: false,
+    // height: 600,
+    // width: 1200,
+  };
+
+  Plotly.newPlot('bubble', data2, layout2);
 });
